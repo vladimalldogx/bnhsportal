9be16d389754a553f7e17a2277fa9e5e31419da9
MEMEMALLEN\Vladimir <shiyocz@gmail.com>
2020-08-22 09:40:48 +0800

    Initial commit

---
+++ application/controllers/home.php
@@ -0,0 +1,9 @@
+<?php
+
+    class home extends CI_Controller{
+        public function index(){
+            $this->load->view('bnhshome');
+        }
+    } 
+           
+?>
---
+++ application/controllers/loginUsers.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class loginUsers extends CI_Controller {
+	//public function __construct() {
+		//parent::__construct();
+		//$this->load->model('userModel');
+	}
+
+	function index()
+	{
+		$this->load->view('loginPage');
+	}
+
+	//function auth() {
+	//	$username  = $this->input->post('username',TRUE);
+	//	$password  = $this->input->post('password',TRUE);
+		//$result    = $this->Login_model->check_user($username, ($password));
+	//	if($result->num_rows() > 0) {
+		//	$data  = $result->row_array();
+		//	$lastname = $data['LastName'];
+			//$firstname = $data['FirstName'];
+			//$mi = $data['MI'];
+			//$name  = $data['username'];
+			//$email = $data['email'];
+		//	$usrtype = $data['usrtype'];
+			//$sesdata = array(
+			//	'LastName' => $lastname,
+			//	'Firstname' => $firstname,
+			//	'MI' => $mi,
+			//	'username'  => $username,
+			//	'email'	   => $email,
+			//	'usrtype'   => $usrtype,
+			//	'logged_in' => TRUE
+			//);
+			//$this->session->set_userdata($sesdata);
+			//if($usrtype === 'ADMIN') {
+				//redirect('Admin');
+			//} elseif($usrtype === 'TEACHER') {
+			//	redirect('teacherController');
+		//	} else {
+			//	redirect('studentController');
+			//}
+		//} else {
+		//	echo "<script>alert('access denied');history.go(-1);</script>";
+		//}
+		//$this->load->view('login_view');
+	//}
+
+	//function logout() {
+	//	$this->session->sess_destroy();
+	//	redirect('loginController');
+//	}
+//}
